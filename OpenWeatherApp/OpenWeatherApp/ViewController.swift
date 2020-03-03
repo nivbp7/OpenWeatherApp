2b237e78b993923ddcc8f7bcf4655e2c0881b79c
niv ben-porath <nbpapps@gmail.com>
2020-03-03 22:37:21 +0200

    created project

---
+++ OpenWeatherApp/OpenWeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OpenWeatherApp
+//
+//  Created by niv ben-porath on 03/03/2020.
+//  Copyright © 2020 nbpApps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
